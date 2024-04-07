90ab045341782ae7436f4dd62697afb020da487f
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-04-08 05:45:50 +0600

    js

---
+++ index.js
@@ -0,0 +1,34 @@
+
+//indexOf
+const arr = [1, 2, 3, 4, 5,6,7];
+//console.log(arr.indexOf(3));
+
+
+
+//join
+console.log(arr.join(',')); 
+
+
+
+// keys
+const keys = arr.keys();
+    console.log(keys);
+
+
+// lastIndexOF
+ console.log('***', arr.lastIndexOf(4));
+
+
+//of
+ const array = [
+    {
+      name: 'li',
+      student : 1,
+    },
+    {
+      name: 'pi',
+      student : 2,
+    },
+  ];
+  
+  
