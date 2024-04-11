55fed9054d34600f56da20ab0223185cf4214fd4
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-04-11 15:44:43 +0600

    js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 //indexOf
 const arr = [1, 2, 3, 4, 5,6,7];
-//console.log(arr.indexOf(3));
+console.log('indexOF',arr.indexOf(3));
 
 
 
 //join
-console.log(arr.join(',')); 
+console.log('join',arr.join(',')); 
 
 
 
 // keys
 const keys = arr.keys();
-    console.log(keys);
+    console.log('keys',keys);
 
 
 // lastIndexOF
- console.log('***', arr.lastIndexOf(4));
+ console.log('lastIndexOF','***', arr.lastIndexOf(4));
 
 
 //of
@@ -30,5 +30,33 @@ const keys = arr.keys();
       student : 2,
     },
   ];
+  console.log('of', array);
   
-  
+
+
+//reduce
+const reduced = arr.reduce((acc, val) => acc + val, 0);
+console.log('reduce', reduced); 
+
+
+
+//shift
+const shift = arr.shift();
+console.log( arr);  
+console.log('shirft',shift);  
+
+
+
+//ToRevesed
+const reversed = arr.slice().reverse();
+console.log('toReversed', reversed); 
+
+
+//toSorted
+const sorted = arr.slice().sort();
+console.log('toSorted', sorted); 
+
+
+//toSpiliced
+const spliced = arr.slice(2);
+console.log('toSpliced', spliced);  
